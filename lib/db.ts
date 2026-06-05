9b56c6b644fba87ef685c2eff10ef5b1840766de
MateoBaute <mateobaute10@gmail.com>
2026-06-05 12:34:16 -0300

    modificacion en db.ts para aiven

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,13 @@ import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '15715'),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    
+    ssl: {
+        rejectUnauthorized: false // <-- ESTA LÍNEA ES OBLIGATORIA PARA AIVEN
+    }
 });
 
 export default db;
